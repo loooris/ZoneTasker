43f762846d60cd3b5eb727eef54a44c0dfee0e68
Tom Droux <tom-christophe.droux@etu.unige.ch>
2023-04-29 11:31:44 +0200

    added Location

--- app/src/main/java/com/github/loooris/zonetasker/MainMenu.kt
+++ app/src/main/java/com/github/loooris/zonetasker/MainMenu.kt
@@ -1,8 +1,11 @@
 package com.github.loooris.zonetasker
 
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -10,11 +13,17 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.github.loooris.zonetasker.databinding.ActivityMainMenuBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
 
 class MainMenu : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMainMenuBinding
+
+    private lateinit var currentLocation : Location
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val permissionCode = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,27 +31,59 @@ class MainMenu : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMainMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        fusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(this)
+
+
+        getCurrentLocationUser()
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+    private fun getCurrentLocationUser(){
+        if(ActivityCompat.checkSelfPermission(
+                this,android.Manifest.permission.ACCESS_FINE_LOCATION)!=
+            PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,android.Manifest.permission.ACCESS_COARSE_LOCATION)!=
+            PackageManager.PERMISSION_GRANTED){
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),permissionCode)
+            return
+        }
+
+        val getLocation= fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+                location ->
+            if(location != null){
+                currentLocation=location
+                Toast.makeText(applicationContext, currentLocation.latitude.toString()+""+
+                        currentLocation.longitude.toString(), Toast.LENGTH_LONG).show()
+
+                val mapFragment = supportFragmentManager
+                    .findFragmentById(R.id.map) as SupportMapFragment
+                mapFragment.getMapAsync(this)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            permissionCode -> if(grantResults.isNotEmpty() && grantResults[0]==
+                PackageManager.PERMISSION_GRANTED){
+                getCurrentLocationUser()
+            }
+        }
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val latLng= LatLng(currentLocation.latitude, currentLocation.longitude)
+        val markerOptions = MarkerOptions().position(latLng).title("Current Location")
+
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+        googleMap.addMarker(markerOptions)
     }
 }
