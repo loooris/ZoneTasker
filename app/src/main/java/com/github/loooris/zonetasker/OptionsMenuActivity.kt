6f24e91213ca1e58b78c5dcac06021386bf263a2
loris.agnolini <loris.agnolini@etu.unige.ch>
2023-05-04 20:14:09 +0200

    Added OptionsMenuActivity

---
+++ app/src/main/java/com/github/loooris/zonetasker/OptionsMenuActivity.kt
@@ -0,0 +1,11 @@
+package com.github.loooris.zonetasker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OptionsMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_options_menu)
+    }
+}
