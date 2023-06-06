9b76c7a99c37f547f4c6f497c00e5c65d4fdbce1
loris.agnolini <loris.agnolini@etu.unige.ch>
2023-06-06 02:27:59 +0200

    add menu buttons and stuff

--- app/src/main/java/com/github/loooris/zonetasker/MainMenuActivity.kt
+++ app/src/main/java/com/github/loooris/zonetasker/MainMenuActivity.kt
@@ -1,11 +1,19 @@
 package com.github.loooris.zonetasker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+        val buttonClick = findViewById<Button>(R.id.MessageButton)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, MapMenu::class.java)
+            startActivity(intent)
+        }
     }
 }
