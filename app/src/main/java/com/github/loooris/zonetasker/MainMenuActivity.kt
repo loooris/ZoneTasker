5633bac3154be8f45ad3f604c79e82291a3fd6f4
loris.agnolini <loris.agnolini@etu.unige.ch>
2023-06-08 00:46:46 +0200

    added message on click

--- app/src/main/java/com/github/loooris/zonetasker/MainMenuActivity.kt
+++ app/src/main/java/com/github/loooris/zonetasker/MainMenuActivity.kt
@@ -4,16 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
 
-        val buttonClick = findViewById<Button>(R.id.ReminderButton)
-        buttonClick.setOnClickListener {
+        val buttonClickReminder = findViewById<Button>(R.id.ReminderButton)
+        buttonClickReminder.setOnClickListener {
             val intent = Intent(this, MapActivity::class.java)
             startActivity(intent)
+        }
+
+        val buttonClickMessage = findViewById<Button>(R.id.MessageButton)
+        buttonClickMessage.setOnClickListener {
+            Toast.makeText(this, "Coming soon!", Toast.LENGTH_SHORT).show()
+        }
+
+        val buttonClickSettings = findViewById<Button>(R.id.SettingsBbutton)
+        buttonClickSettings.setOnClickListener {
+            Toast.makeText(this, "Coming soon!", Toast.LENGTH_SHORT).show()
         }
     }
 }
